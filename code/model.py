b5a095783a17545f25e13f54ddb00906cfc4673a
Jeongwhan Choi <jeongwhan.choi@yonsei.ac.kr>
2021-11-01 11:30:45 +0900

    Remove comments

--- code/model.py
+++ code/model.py
@@ -1,5 +1,6 @@
 """
-Define models here
+# LT-OCF model in PyTorch
+
 """
 import world
 import torch
@@ -7,8 +8,6 @@ from dataloader import BasicDataset
 import dataloader
 from torch import nn
 import odeblock as ode
-
-from labml import monit, tracker, logger, experiment
 
 class BasicModel(nn.Module):    
     def __init__(self):
@@ -21,14 +20,6 @@ class PairWiseModel(BasicModel):
     def __init__(self):
         super(PairWiseModel, self).__init__()
     def bpr_loss(self, users, pos, neg):
-        """
-        Parameters:
-            users: users list 
-            pos: positive items for corresponding users
-            neg: negative items for corresponding users
-        Return:
-            (log-loss, l2-loss)
-        """
         raise NotImplementedError
     
 class PureMF(BasicModel):
@@ -97,10 +88,6 @@ class LightGCN(BasicModel):
         self.embedding_item = torch.nn.Embedding(
             num_embeddings=self.num_items, embedding_dim=self.latent_dim)
         if self.config['pretrain'] == 0:
-#             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
-#             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
-#             print('use xavier initilizer')
-# random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
             nn.init.normal_(self.embedding_user.weight, std=0.1)
             nn.init.normal_(self.embedding_item.weight, std=0.1)
             world.cprint('use NORMAL distribution initilizer')
@@ -111,8 +98,6 @@ class LightGCN(BasicModel):
         self.f = nn.Sigmoid()
         self.Graph = self.dataset.getSparseGraph()
         print(f"lgn is already to go(dropout:{self.config['dropout']})")
-
-        # print("save_txt")
     def __dropout_x(self, x, keep_prob):
         size = x.size()
         index = x.indices().t()
@@ -140,7 +125,6 @@ class LightGCN(BasicModel):
         users_emb = self.embedding_user.weight
         items_emb = self.embedding_item.weight
         all_emb = torch.cat([users_emb, items_emb])
-        #   torch.split(all_emb , [self.num_users, self.num_items])
         embs = [all_emb]
         if self.config['dropout']:
             if self.training:
@@ -161,7 +145,6 @@ class LightGCN(BasicModel):
                 all_emb = torch.sparse.mm(g_droped, all_emb)
             embs.append(all_emb)
         embs = torch.stack(embs, dim=1)
-        #print(embs.size())
         light_out = torch.mean(embs, dim=1)
         users, items = torch.split(light_out, [self.num_users, self.num_items])
         return users, items
@@ -199,9 +182,7 @@ class LightGCN(BasicModel):
         return loss, reg_loss
        
     def forward(self, users, items):
-        # compute embedding
         all_users, all_items = self.computer()
-        #all_users, all_items = self.computer()
         users_emb = all_users[users]
         items_emb = all_items[items]
         inner_pro = torch.mul(users_emb, items_emb)
@@ -232,16 +213,10 @@ class LTOCF(BasicModel):
         self.embedding_item = torch.nn.Embedding(
             num_embeddings=self.num_items, embedding_dim=self.latent_dim)
         if self.config['pretrain'] == 0:
-#             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
-#             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
-#             print('use xavier initilizer')
-# random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
             nn.init.normal_(self.embedding_user.weight, std=0.1)
             nn.init.normal_(self.embedding_item.weight, std=0.1)
             world.cprint('use NORMAL distribution initilizer')
         else:
-            # self.embedding_user.weight.data.copy_(torch.from_numpy(self.config['user_emb']))
-            # self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
             print('use pretarined data')
         self.f = nn.Sigmoid()
         self.Graph = self.dataset.getSparseGraph()
@@ -343,337 +318,6 @@ class LTOCF(BasicModel):
             all_emb_4 = self.ode_block_4(all_emb_3)
             all_emb_4 = all_emb_4 - all_emb_3
             embs.append(all_emb_4)
-
-        embs = torch.stack(embs, dim=1)
-        light_out = torch.mean(embs, dim=1)        
-
-        users, items = torch.split(light_out, [self.num_users, self.num_items])
-        return users, items
-    
-    def getUsersRating(self, users):
-        all_users, all_items = self.computer()
-        users_emb = all_users[users.long()]
-        items_emb = all_items
-        rating = self.f(torch.matmul(users_emb, items_emb.t()))
-        return rating
-    
-    def getEmbedding(self, users, pos_items, neg_items):
-        all_users, all_items = self.computer()
-        users_emb = all_users[users]
-        pos_emb = all_items[pos_items]
-        neg_emb = all_items[neg_items]
-        users_emb_ego = self.embedding_user(users)
-        pos_emb_ego = self.embedding_item(pos_items)
-        neg_emb_ego = self.embedding_item(neg_items)
-        return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
-    
-    def bpr_loss(self, users, pos, neg):
-        (users_emb, pos_emb, neg_emb, 
-        userEmb0,  posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
-        reg_loss = (1/2)*(userEmb0.norm(2).pow(2) + 
-                         posEmb0.norm(2).pow(2)  +
-                         negEmb0.norm(2).pow(2))/float(len(users))
-        pos_scores = torch.mul(users_emb, pos_emb)
-        pos_scores = torch.sum(pos_scores, dim=1)
-        neg_scores = torch.mul(users_emb, neg_emb)
-        neg_scores = torch.sum(neg_scores, dim=1)
-        
-        loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
-        
-        return loss, reg_loss
-       
-    def forward(self, users, items):
-        # compute embedding
-        all_users, all_items = self.computer()
-        users_emb = all_users[users]
-        items_emb = all_items[items]
-        inner_pro = torch.mul(users_emb, items_emb)
-        gamma     = torch.sum(inner_pro, dim=1)
-        return gamma
-
-class LTOCF2(BasicModel):
-    def __init__(self, 
-                 config:dict, 
-                 dataset:BasicDataset):
-        super(LTOCF2, self).__init__()
-        self.config = config
-        self.dataset : dataloader.BasicDataset = dataset
-        self.__init_weight()
-        self.__init_ode()
-
-
-    def __init_weight(self):
-        self.num_users  = self.dataset.n_users
-        self.num_items  = self.dataset.m_items
-        self.latent_dim = self.config['latent_dim_rec']
-        self.n_layers = self.config['lightGCN_n_layers']
-        self.keep_prob = self.config['keep_prob']
-        self.A_split = self.config['A_split']
-        self.embedding_user = torch.nn.Embedding(
-            num_embeddings=self.num_users, embedding_dim=self.latent_dim)
-        self.embedding_item = torch.nn.Embedding(
-            num_embeddings=self.num_items, embedding_dim=self.latent_dim)
-        if self.config['pretrain'] == 0:
-#             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
-#             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
-#             print('use xavier initilizer')
-# random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
-            nn.init.normal_(self.embedding_user.weight, std=0.1)
-            nn.init.normal_(self.embedding_item.weight, std=0.1)
-            world.cprint('use NORMAL distribution initilizer')
-        else:
-            # self.embedding_user.weight.data.copy_(torch.from_numpy(self.config['user_emb']))
-            # self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
-            print('use pretarined data')
-        self.f = nn.Sigmoid()
-        self.Graph = self.dataset.getSparseGraph()
-        print(f"lgn is already to go(dropout:{self.config['dropout']})")
-    
-    def __init_ode(self):
-        if world.config['learnable_time'] == True:
-            self.time_split = self.config['time_split'] # init the number of time split
-
-            self.odetimes = ode.ODETimeSetter(self.time_split, self.config['K'])
-            self.odetime_1 = [self.odetimes[0]]
-            self.odetime_2 = [self.odetimes[1]]
-            self.ode_block_test_1 = ode.ODEBlockTimeFirst(ode.ODEFunction(self.Graph),self.time_split, self.config['solver'])
-            self.ode_block_test_2 = ode.ODEBlockTimeMiddle(ode.ODEFunction(self.Graph),self.time_split, self.config['solver'])
-            self.ode_block_test_3 = ode.ODEBlockTimeLastK(ode.ODEFunction(self.Graph),self.time_split, self.config['solver'],  self.config['K'])
-        else:
-            self.ode_block_1 = ode.ODEBlock(ode.ODEFunction(self.Graph), self.config['solver'], 0, self.config['K']/3)
-            self.ode_block_2 = ode.ODEBlock(ode.ODEFunction(self.Graph), self.config['solver'], self.config['K']/3, 2*self.config['K']/3)
-            self.ode_block_3 = ode.ODEBlock(ode.ODEFunction(self.Graph), self.config['solver'], 2*self.config['K']/3, self.config['K'])
-
-    def get_time(self):
-        ode_times=list(self.odetime_1)+ list(self.odetime_2)
-        return ode_times
-        
-    def __dropout_x(self, x, keep_prob):
-        size = x.size()
-        index = x.indices().t()
-        values = x.values()
-        random_index = torch.rand(len(values)) + keep_prob
-        random_index = random_index.int().bool()
-        index = index[random_index]
-        values = values[random_index]/keep_prob
-        g = torch.sparse.FloatTensor(index.t(), values, size)
-        return g
-    
-    def __dropout(self, keep_prob):
-        if self.A_split:
-            graph = []
-            for g in self.Graph:
-                graph.append(self.__dropout_x(g, keep_prob))
-        else:
-            graph = self.__dropout_x(self.Graph, keep_prob)
-        return graph
-    
-    def computer(self):
-        """
-        propagate methods for Continuous-Time lightGCN
-        """       
-        users_emb = self.embedding_user.weight
-        items_emb = self.embedding_item.weight
-        all_emb = torch.cat([users_emb, items_emb])
-        embs = [all_emb]
-        if self.config['dropout']:
-            if self.training:
-                g_droped = self.__dropout(self.keep_prob)
-            else:
-                g_droped = self.Graph        
-        else:
-            g_droped = self.Graph    
-        
-        """
-        layers
-        """
-        if world.config['learnable_time'] == True:
-            out_1 = self.ode_block_test_1(all_emb, self.odetime_1)
-            if world.config['dual_res'] == False:
-                out_1 = out_1 - all_emb
-            embs.append(out_1)
-
-            out_2 = self.ode_block_test_2(out_1, self.odetime_1, self.odetime_2)
-            if world.config['dual_res'] == False:
-                out_2 = out_2 - out_1
-            embs.append(out_2)
-
-            out_3 = self.ode_block_test_3(out_2, self.odetime_2)
-            if world.config['dual_res'] == False:
-                out_3 = out_3 - out_2
-            embs.append(out_3)            
-
-            
-        elif world.config['learnable_time'] == False:
-            all_emb_1 = self.ode_block_1(all_emb)
-            all_emb_1 = all_emb_1 - all_emb
-            embs.append(all_emb_1)
-            all_emb_2 = self.ode_block_2(all_emb_1)
-            all_emb_2 = all_emb_2 - all_emb_1
-            embs.append(all_emb_2)
-            all_emb_3 = self.ode_block_3(all_emb_2)
-            all_emb_3 = all_emb_3 - all_emb_2
-            embs.append(all_emb_3)
-
-        embs = torch.stack(embs, dim=1)
-        light_out = torch.mean(embs, dim=1)        
-
-        users, items = torch.split(light_out, [self.num_users, self.num_items])
-        return users, items
-    
-    def getUsersRating(self, users):
-        all_users, all_items = self.computer()
-        users_emb = all_users[users.long()]
-        items_emb = all_items
-        rating = self.f(torch.matmul(users_emb, items_emb.t()))
-        return rating
-    
-    def getEmbedding(self, users, pos_items, neg_items):
-        all_users, all_items = self.computer()
-        users_emb = all_users[users]
-        pos_emb = all_items[pos_items]
-        neg_emb = all_items[neg_items]
-        users_emb_ego = self.embedding_user(users)
-        pos_emb_ego = self.embedding_item(pos_items)
-        neg_emb_ego = self.embedding_item(neg_items)
-        return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
-    
-    def bpr_loss(self, users, pos, neg):
-        (users_emb, pos_emb, neg_emb, 
-        userEmb0,  posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
-        reg_loss = (1/2)*(userEmb0.norm(2).pow(2) + 
-                         posEmb0.norm(2).pow(2)  +
-                         negEmb0.norm(2).pow(2))/float(len(users))
-        pos_scores = torch.mul(users_emb, pos_emb)
-        pos_scores = torch.sum(pos_scores, dim=1)
-        neg_scores = torch.mul(users_emb, neg_emb)
-        neg_scores = torch.sum(neg_scores, dim=1)
-        
-        loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
-        
-        return loss, reg_loss
-       
-    def forward(self, users, items):
-        # compute embedding
-        all_users, all_items = self.computer()
-        users_emb = all_users[users]
-        items_emb = all_items[items]
-        inner_pro = torch.mul(users_emb, items_emb)
-        gamma     = torch.sum(inner_pro, dim=1)
-        return gamma
-
-class LTOCF1(BasicModel):
-    def __init__(self, 
-                 config:dict, 
-                 dataset:BasicDataset):
-        super(LTOCF1, self).__init__()
-        self.config = config
-        self.dataset : dataloader.BasicDataset = dataset
-        self.__init_weight()
-        self.__init_ode()
-
-
-    def __init_weight(self):
-        self.num_users  = self.dataset.n_users
-        self.num_items  = self.dataset.m_items
-        self.latent_dim = self.config['latent_dim_rec']
-        self.n_layers = self.config['lightGCN_n_layers']
-        self.keep_prob = self.config['keep_prob']
-        self.A_split = self.config['A_split']
-        self.embedding_user = torch.nn.Embedding(
-            num_embeddings=self.num_users, embedding_dim=self.latent_dim)
-        self.embedding_item = torch.nn.Embedding(
-            num_embeddings=self.num_items, embedding_dim=self.latent_dim)
-        if self.config['pretrain'] == 0:
-#             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
-#             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
-#             print('use xavier initilizer')
-# random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
-            nn.init.normal_(self.embedding_user.weight, std=0.1)
-            nn.init.normal_(self.embedding_item.weight, std=0.1)
-            world.cprint('use NORMAL distribution initilizer')
-        else:
-            # self.embedding_user.weight.data.copy_(torch.from_numpy(self.config['user_emb']))
-            # self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
-            print('use pretarined data')
-        self.f = nn.Sigmoid()
-        self.Graph = self.dataset.getSparseGraph()
-        print(f"lgn is already to go(dropout:{self.config['dropout']})")
-    
-    def __init_ode(self):
-        if world.config['learnable_time'] == True:
-            self.time_split = self.config['time_split'] # init the number of time split
-
-            self.odetimes = ode.ODETimeSetter(self.time_split, self.config['K'])
-            self.odetime_1 = [self.odetimes[0]]
-            self.ode_block_test_1 = ode.ODEBlockTimeFirst(ode.ODEFunction(self.Graph),self.time_split, self.config['solver'])
-            self.ode_block_test_2 = ode.ODEBlockTimeLastK(ode.ODEFunction(self.Graph),self.time_split, self.config['solver'], self.config['K'])
-        else:
-            self.ode_block_1 = ode.ODEBlock(ode.ODEFunction(self.Graph), self.config['solver'], 0, self.config['K']/2)
-            self.ode_block_2 = ode.ODEBlock(ode.ODEFunction(self.Graph), self.config['solver'], self.config['K']/2, self.config['K'])
-
-    def get_time(self):
-        ode_times=list(self.odetime_1)
-        return ode_times
-        
-    def __dropout_x(self, x, keep_prob):
-        size = x.size()
-        index = x.indices().t()
-        values = x.values()
-        random_index = torch.rand(len(values)) + keep_prob
-        random_index = random_index.int().bool()
-        index = index[random_index]
-        values = values[random_index]/keep_prob
-        g = torch.sparse.FloatTensor(index.t(), values, size)
-        return g
-    
-    def __dropout(self, keep_prob):
-        if self.A_split:
-            graph = []
-            for g in self.Graph:
-                graph.append(self.__dropout_x(g, keep_prob))
-        else:
-            graph = self.__dropout_x(self.Graph, keep_prob)
-        return graph
-    
-    def computer(self):
-        """
-        propagate methods for Continuous-Time lightGCN
-        """       
-        users_emb = self.embedding_user.weight
-        items_emb = self.embedding_item.weight
-        all_emb = torch.cat([users_emb, items_emb])
-        embs = [all_emb]
-        if self.config['dropout']:
-            if self.training:
-                g_droped = self.__dropout(self.keep_prob)
-            else:
-                g_droped = self.Graph        
-        else:
-            g_droped = self.Graph    
-        
-        """
-        layers
-        """
-        if world.config['learnable_time'] == True:
-            out_1 = self.ode_block_test_1(all_emb, self.odetime_1)
-            if world.config['dual_res'] == False:
-                out_1 = out_1 - all_emb
-            embs.append(out_1)
-
-            out_2 = self.ode_block_test_2(out_1, self.odetime_1)
-            if world.config['dual_res'] == False:
-                out_2 = out_2 - out_1
-            embs.append(out_2)       
-
-            
-        elif world.config['learnable_time'] == False:
-            all_emb_1 = self.ode_block_1(all_emb)
-            all_emb_1 = all_emb_1 - all_emb
-            embs.append(all_emb_1)
-            all_emb_2 = self.ode_block_2(all_emb_1)
-            all_emb_2 = all_emb_2 - all_emb_1
-            embs.append(all_emb_2)
 
         embs = torch.stack(embs, dim=1)
         light_out = torch.mean(embs, dim=1)        
